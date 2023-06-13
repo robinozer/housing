47c75e7c9c463edb5c95a10832f310354a9dbbdd
robinozer <>
2023-06-13 16:57:57 +0000

    reformat streamlit page after inconsistent use of tabs and indents were causing an issue

--- app_pages/page_predict_house_price.py
+++ app_pages/page_predict_house_price.py
@@ -1,12 +1,7 @@
 import streamlit as st
-from src.data_management import (
-    load_housing_data,
-    load_pkl_file,
-    load_cleaned_inherited_houses_data,
-)
+from src.data_management import load_housing_data, load_pkl_file
 import numpy as np
 import pandas as pd
-
 
 def page_predict_house_price_body():
 
@@ -20,102 +15,97 @@ def page_predict_house_price_body():
     ).columns.to_list()
 
     # this is the inherited houses (cleaned to have matching data types with the main dataset)
-    inherited_df = pd.read_csv(
-        "outputs/datasets/cleaned/inherited_houses_cleaned.csv")
+    inherited_df = pd.read_csv("outputs/datasets/cleaned/inherited_houses_cleaned.csv")
 
-	# predict prices of inherited houses with ML pipeline from 05-Regression pipeline notebook	
+    # predict prices of inherited houses with ML pipeline from 05-Regression pipeline notebook
     st.write("### House sale prices from client's inherited houses")
-	st.info(f"* The table below shows the four inherited houses profile")
-	st.write(inherited_df.head())
-	inherited_df = inherited_df.filter(best_features)
-	house_price_prediction = pipeline.predict(inherited_df).round(0)
-	inherited_df['Predicted House Sale Price'] = house_price_prediction
-	st.write(
+    st.info(f"* The table below shows the four inherited houses profile")
+    
+    st.write(inherited_df.head())
+    inherited_df = inherited_df.filter(best_features)
+    house_price_prediction = pipeline.predict(inherited_df).round(0)
+    inherited_df['Predicted House Sale Price'] = house_price_prediction
+    st.write(
         f"* The table below shows the predicted sale prices for the four houses, together with the house features used in the prediction, "
-		"which are the two most important variables we saw in the House Price Study page: 'Overall Quality' and 'Above Ground Living "
-		"Area Square Feet'."
-	)
-	st.write(inherited_df.head())
+        f"which are the four most important variables we saw in the House Price Study page: 'OverallQuality', 'TotalBsmtSF', '2ndFlrSF' and 'GarageArea'."
+    )
+    st.write(inherited_df.head())
 
-	# calculate sum of inherited houses predicted prices
-	sum = inherited_df['Predicted House Sale Price'].sum()
-	st.write(
+    # calculate sum of inherited houses predicted prices
+    sum = inherited_df['Predicted House Sale Price'].sum()
+    st.write(
         f"* The sum of the predicted sale prices for the four houses is: &nbsp; &nbsp; &nbsp;{sum}  \n"
-	)
+    )
 
-	st.write("---")
+    st.write("---")
 
-	# predict price of any other house in Ames, Iowa
-	st.write("### Predict house sale prices in Ames, Iowa  \n")
-	st.write("* The following 4 variables 'Overall Quality',  "
-	"are needed for the ML model to predict the price.")
-	st.warning("* The model has limitations: For example the maximum 'Above Ground Living Area Square Feet'"
-	" in the train set is 5642 square feet and one should not expect the model to generalize to "
-	"larger areas.")
-	# create input fields for live data
-	X_live = DrawInputsWidgets()
-	# predict on live data
-	if st.button("Run Predictive Analysis"):
-		house_price_prediction = pipeline.predict(X_live.filter(best_features)).round(0)
-		st.write(
-			f"* The predicted sale price for the house is: &nbsp; &nbsp; &nbsp;{house_price_prediction[0]}  \n"
-		)
-			
+    # predict price of any other house in Ames, Iowa
+    st.write("### Predict house sale prices in Ames, Iowa  \n")
+    st.write("* The following 4 variables 'Overall Quality',  "
+             "'TotalBsmtSF', '2ndFlrSF', and 'GarageArea' are needed for the ML model to predict the price.")
+
+    # create input fields for live data
+    X_live = DrawInputsWidgets()
+    # predict on live data
+    if st.button("Run Predictive Analysis"):
+        house_price_prediction = pipeline.predict(X_live.filter(best_features)).round(0)
+        st.write(
+            f"* The predicted sale price for the house is: &nbsp; &nbsp; &nbsp;{house_price_prediction[0]}  \n"
+        )
 
 def DrawInputsWidgets():
 
-	df = load_housing_data()
-	percentageMin, percentageMax = 0.5, 1.0
+    df = load_housing_data()
+    percentageMin, percentageMax = 0.5, 1.0
 
-    # we create input widgets for 4 features	
-	col1, col2, col3, col4 = st.beta_columns(4)
+    # we create input widgets for 4 features
+    col1, col2, col3, col4 = st.beta_columns(4)
 
-	# We are using these features to feed the ML pipeline
-		
- 	# create an empty DataFrame, which will be the live data
-	X_live = pd.DataFrame([], index=[0])
-	
-	# from here on we draw the widget based on the variable type (numerical or categorical)
-	# and set initial values
-	with col1:
-		feature = 'OverallQual'
-		st_widget = st.number_input(
-	 		label= feature,
-			min_value= df[feature].min()*percentageMin,
-			max_value= df[feature].max()*percentageMax,
-			value= df[feature].median()
-			)
-	X_live[feature] = st_widget
+    # We are using these features to feed the ML pipeline
 
-	with col2:
-		feature = 'TotalBsmtSF'
-		st_widget = st.number_input(
-	 		label= feature,
-			min_value= df[feature].min()*percentageMin,
-			max_value= df[feature].max()*percentageMax,
-			value= df[feature].median()
-			)
-	X_live[feature] = st_widget
+    # create an empty DataFrame, which will be the live data
+    X_live = pd.DataFrame([], index=[0])
 
-	with col3:
-		feature = '2ndFlrSF'
-		st_widget = st.number_input(
-	 		label= feature,
-			min_value= df[feature].min()*percentageMin,
-			max_value= df[feature].max()*percentageMax,
-			value= df[feature].median()
-			)
-	X_live[feature] = st_widget
-    
-	with col4:
-		feature = 'GarageArea'
-		st_widget = st.number_input(
-	 		label= feature,
-			min_value= df[feature].min()*percentageMin,
-			max_value= df[feature].max()*percentageMax,
-			value= df[feature].median()
-			)
-	X_live[feature] = st_widget  
+    # from here on we draw the widget based on the variable type (numerical or categorical)
+    # and set initial values
+    with col1:
+        feature = 'OverallQual'
+        st_widget = st.number_input(
+            label=feature,
+            min_value=df[feature].min() * percentageMin,
+            max_value=df[feature].max() * percentageMax,
+            value=df[feature].median()
+        )
+        X_live[feature] = st_widget
 
-	return X_live
-    
+    with col2:
+        feature = 'TotalBsmtSF'
+        st_widget = st.number_input(
+            label=feature,
+            min_value=df[feature].min() * percentageMin,
+            max_value=df[feature].max() * percentageMax,
+            value=df[feature].median()
+        )
+        X_live[feature] = st_widget
+
+    with col3:
+        feature = '2ndFlrSF'
+        st_widget = st.number_input(
+            label=feature,
+            min_value=df[feature].min() * percentageMin,
+            max_value=df[feature].max() * percentageMax,
+            value=df[feature].median()
+        )
+        X_live[feature] = st_widget
+
+    with col4:
+        feature = 'GarageArea'
+        st_widget = st.number_input(
+            label=feature,
+            min_value=df[feature].min() * percentageMin,
+            max_value=df[feature].max() * percentageMax,
+            value=df[feature].median()
+        )
+        X_live[feature] = st_widget
+
+    return X_live
