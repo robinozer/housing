af9d1cd4bd979475a559662bb3f97f6513376b73
robinozer <>
2023-06-14 21:49:20 +0000

    edit streamlit page and delete v1 of pipeline and data as not needed

--- src/machine_learning/predictive_analysis_ui.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import joblib
-from src.data_management import load_cleaned_housing_data
-from src.converters import ConvertToInt64
-
-
-def page_predict_house_price_body():
-pass
