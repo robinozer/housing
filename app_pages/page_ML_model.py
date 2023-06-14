790adcf9c300b97c096e2e6ded88ae9e2796fd9d
robinozer <>
2023-06-14 22:18:09 +0000

    delete duplicated notebook, change function for regression evaluation in order to display df with data  metrics on streamlit page

--- app_pages/page_ML_model.py
+++ app_pages/page_ML_model.py
@@ -6,22 +6,21 @@ from src.evaluate_rgr import regression_performance, regression_evaluation, regr
 
 
 def page_ML_model_body():
-
     # load house price pipeline files
     version = 'v3'
     pipeline = load_pkl_file(f"outputs/ml_pipeline/predict_housing/{version}/best_regressor_pipeline.pkl")
     feat_importance = plt.imread(f"outputs//ml_pipeline/predict_housing/{version}/features_importance.png")
     X_train = pd.read_csv(f"outputs//ml_pipeline/predict_housing/{version}/X_train.csv")
     X_test = pd.read_csv(f"outputs//ml_pipeline/predict_housing/{version}/X_test.csv")
-    y_train =  pd.read_csv(f"outputs//ml_pipeline/predict_housing/{version}/y_train.csv").squeeze()
-    y_test =  pd.read_csv(f"outputs//ml_pipeline/predict_housing/{version}/y_test.csv").squeeze()
+    y_train = pd.read_csv(f"outputs//ml_pipeline/predict_housing/{version}/y_train.csv").squeeze()
+    y_test = pd.read_csv(f"outputs//ml_pipeline/predict_housing/{version}/y_test.csv").squeeze()
 
     st.write("### ML pipeline")
 
     # summary of model performance
     st.info(
         f"* An R2 score of at least 0.8 on both train and test sets was acceptable to the client."
-        f"* This pipeline achieves 0.906 on the train set and and 0.80 on the train set and test set respectively  \n"    
+        f"* This pipeline achieves 0.906 on the train set and and 0.832 on the test set.  \n"    
     )
     st.write("---")
 
@@ -38,10 +37,23 @@ def page_ML_model_body():
     st.write("---")
 
     # evaluate performance on train and test sets
-    regression_performance(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, pipeline=pipeline)
-    
+    train_evaluation = regression_evaluation(X_train, y_train, pipeline)
+    test_evaluation = regression_evaluation(X_test, y_test, pipeline)
+
     st.write("---")
-     
+
+    # display evaluation table for train set
+    st.write("* **Evaluation Metrics for Train Set**")
+    st.dataframe(train_evaluation)
+
+    st.write("---")
+
+    # display evaluation table for test set
+    st.write("* **Evaluation Metrics for Test Set**")
+    st.dataframe(test_evaluation)
+
+    st.write("---")
+
     # Plot predicted versus actual sale price for train and test sets
     st.write("* **Predicted versus actual sale price scatterplot**")
     regression_evaluation_plots(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, pipeline=pipeline, alpha_scatter=0.5)
--- src/evaluate_rgr.py
+++ src/evaluate_rgr.py
@@ -14,14 +14,32 @@ def regression_performance(X_train, y_train, X_test, y_test, pipeline):
     regression_evaluation(X_test, y_test, pipeline)
 
 
-def regression_evaluation(X, y, pipeline):
+"""def regression_evaluation(X, y, pipeline):
     prediction = pipeline.predict(X)
     print('R2 Score:', r2_score(y, prediction).round(3))
     print('Mean Absolute Error:', mean_absolute_error(y, prediction).round(3))
     print('Mean Squared Error:', mean_squared_error(y, prediction).round(3))
     print('Root Mean Squared Error:', np.sqrt(
         mean_squared_error(y, prediction)).round(3))
-    print("\n")
+    print("\n")"""
+
+def regression_evaluation(X, y, pipeline):
+    prediction = pipeline.predict(X)
+    r2 = r2_score(y, prediction).round(3)
+    mae = mean_absolute_error(y, prediction).round(3)
+    mse = mean_squared_error(y, prediction).round(3)
+    rmse = np.sqrt(mean_squared_error(y, prediction)).round(3)
+
+    evaluation_df = pd.DataFrame(
+        {
+            "R2 Score": [r2],
+            "Mean Absolute Error": [mae],
+            "Mean Squared Error": [mse],
+            "Root Mean Squared Error": [rmse],
+        }
+    )
+
+    return evaluation_df
 
 
 def regression_evaluation_plots(X_train, y_train, X_test, y_test, pipeline, alpha_scatter=0.5):
