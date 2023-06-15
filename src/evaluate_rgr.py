f26d27b2d093aa8361dd5cbf30ae1dc896ea9dbb
robinozer <>
2023-06-15 07:54:20 +0000

    add explanatory note to evaluate rgr file and add altair to requirements.txt

--- src/evaluate_rgr.py
+++ src/evaluate_rgr.py
@@ -13,15 +13,19 @@ def regression_performance(X_train, y_train, X_test, y_test, pipeline):
     print("* Test Set")
     regression_evaluation(X_test, y_test, pipeline)
 
-
-"""def regression_evaluation(X, y, pipeline):
+# Here is the original code from notebook '05-Regression Pipeline'
+# for evaluation. It has been edited below to show a df with evaluation metrics
+# on Streamlit.
+"""
+def regression_evaluation(X, y, pipeline):
     prediction = pipeline.predict(X)
     print('R2 Score:', r2_score(y, prediction).round(3))
     print('Mean Absolute Error:', mean_absolute_error(y, prediction).round(3))
     print('Mean Squared Error:', mean_squared_error(y, prediction).round(3))
     print('Root Mean Squared Error:', np.sqrt(
         mean_squared_error(y, prediction)).round(3))
-    print("\n")"""
+    print("\n")
+"""
 
 def regression_evaluation(X, y, pipeline):
     prediction = pipeline.predict(X)
@@ -41,7 +45,7 @@ def regression_evaluation(X, y, pipeline):
 
     return evaluation_df
 
-
+# Code copied from notebook '05-Regression Pipeline'
 def regression_evaluation_plots(X_train, y_train, X_test, y_test, pipeline, alpha_scatter=0.5):
     pred_train = pipeline.predict(X_train)
     pred_test = pipeline.predict(X_test)
